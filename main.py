2c595c02ae1f761e0a68e7b3c0447fa6ff95589b
christgaillard <christ.gaillard@gmail.com>
2018-10-27 17:28:45 +0200

    premiere intégration du capteur ultrason droit

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 import numpy as np
 import cv2
-
+import RPi.GPIO as GPIO
+import time, sys
 
 cap = cv2.VideoCapture(0)
+
+
+trigger_pin_left = 8
+echo_pin_left = 7
+
+trigger_pin_right=18
+echo_pin_right = 23
+
+GPIO.setmode (GPIO.BCM)
+GPIO.setup(trigger_pin_right, GPIO.OUT)
+GPIO.setup(echo_pin_right, GPIO.IN)
+
+def send_trigger_pulse(pin):
+    GPIO.output(pin, True)
+    time.sleep(0.0001)
+    GPIO.output(pin,False)
+    
+def wait_for_echo(pin, value, timeout):
+    count = timeout
+    while GPIO.input(pin) != value and count > 0:
+        count  -= 1
+
+def get_distance(trigger_pin, echo_pin):
+    send_trigger_pulse(trigger_pin)
+    wait_for_echo(echo_pin, True, 10000)
+    start = time.time()
+    wait_for_echo(echo_pin, False,10000)
+    finish = time.time()
+    pulse_len = finish - start
+    distance_cm = pulse_len/0.000058
+    return int(distance_cm)
 
 green = (0, 255, 0)
 orange = (0, 255, 255)
@@ -14,6 +46,10 @@ while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
+    #left_distance = get_distance(trigger_pin_lefft, echo_pin_left)
+    right_distance = get_distance(trigger_pin_right,echo_pin_right) 
+
+
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -23,14 +59,17 @@ while(True):
     cv2.line(frame,(90,400),(110,400),red,3)
     cv2.line(frame, (490, 400), (510, 400), red, 3)
     cv2.line(frame,(100,397),(185,300),orange,5)
-    #font = cv2.FONT_HERSHEY_SIMPLEX
-    # cv2.putText(frame, 'OpenCV', (10, 400), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame, 'distance Objet : '+ str(right_distance), (10, 400), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+    
     # Display the resulting frame
     cv2.imshow('video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        GPIO.cleanup()
         break
 
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+time.sleep(0,1)
