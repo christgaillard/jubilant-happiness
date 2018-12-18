e8de261f985420341a9981c99623c08bf602395d
Christophe Gaillard <cgai@administrateur-n751jk.brocelia>
2018-12-18 07:31:54 +0100

    ajout de bouttons, et utilisation de grille

--- interface/window.py
+++ interface/window.py
@@ -10,16 +10,21 @@ class App:
         self.window.title(window_title)
         self.video_source = video_source
 
-        #self.photo = tkinter.PhotoImage(file='images/bt_fermer.gif')
+        # self.photo = tkinter.PhotoImage(file='images/bt_fermer.gif')
         self.helv22 = tkFont.Font(family='Helvetica', size=22, weight=tkFont.BOLD)
 
         self.vid = cap.MyvideoCapture(self.video_source)
 
-        self.bouton= tkinter.Button(window, text="Fermer", font=self.helv22, cursor='hand1', command=window.quit)
-        self.bouton.pack()
+        self.boutonClose= tkinter.Button(window, text="Fermer", font=self.helv22, cursor='hand1', command=window.quit)
+        # self.boutonClose.pack(side="top", expand=False, padx=4, pady=4, anchor="w")
+        self.boutonClose.grid(row=0, column=0)
 
-        self.canvas = tkinter.Canvas(window, width=850, height=600)
-        self.canvas.pack()
+        self.boutonShowLine = tkinter.Button(window, text='Line détection', font=self.helv22, cursor='hand1', command='')
+        # self.boutonShowLine.pack(side="top", expand=False, padx=4, pady=4)
+        self.boutonShowLine.grid(row=0, column=1)
+
+        self.canvas = tkinter.Canvas(window, width=800, height=480)
+        self.canvas.grid(row=1, column=0, columnspan=2)
 
         self.delay = 15
         self.update()
@@ -34,4 +39,3 @@ class App:
             self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW)
 
         self.window.after(self.delay, self.update)
-
